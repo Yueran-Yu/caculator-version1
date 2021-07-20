9bb28bdd8caff066e1ff49f1b21486a8f051ffca
GraceYu <librayuyueran@gmail.com>
2021-07-19 20:03:11 -0500

    complete one caculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState('')
+  const handleClick = (e) => {
+    const text = e.target.textContent
+
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        setResult(result.concat(e.target.textContent))
+        break;
+      case 'Clear':
+        setResult('')
+        break;
+      case 'C':
+        setResult(result.slice(0, result.length - 1))
+        break;
+      case '=':
+        try {
+          setResult(eval(result).toString())
+        } catch (err) {
+          setResult('Error')
+        }
+        break;
+      default:
+        console.log(text)
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <form>
+        <input type='text' value={result}/>
+      </form>
+      <div className="keypad " onClick={handleClick}>
+        <button className='clear highlight'>Clear</button>
+        <button className='backspace highlight '>C</button>
+        <button className='highlight'>&divide;</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='highlight'>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className='highlight'>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className='highlight'>+</button>
+        <button>0</button>
+        <button className='highlight'>.</button>
+        <button className='equal highlight'>=</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
